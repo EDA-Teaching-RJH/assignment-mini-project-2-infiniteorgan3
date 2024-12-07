70536d9a1f99d820a60245b859433bb84f83dfa9
Ellie Marsden <ejam2@kent.ac.uk>
2024-12-07 11:52:08 +0000

    sss

--- MiniProjectPart2.py
+++ MiniProjectPart2.py
@@ -3,31 +3,57 @@ import re
 
 listofusers = []
 def createaccount():
+    reasonsforinvalid = []
+    attempts = 3
     username = ""
-    while validateusername(username) == False:
+    while type(validateusername(username)) == list and attempts > 0:
+        reasonsforinvalid = validateusername(username)
+        for i in reasonsforinvalid:
+            print(i)
         username = input("Please enter a username for the account:\n").strip()
+        attempts -= 1
     password = ""
-    while validatepassword(password) == False:
+    while type(validatepassword(password)) == list and attempts > 0:
+        reasonsforinvalid = validatepassword(password)
+        for i in reasonsforinvalid:
+            print(i)
         password = input("Please enter the password for the account:\n")
+        attempts -= 1
     email = ""
-    while validateemail(email):
+    while type(validateemail(email)) == list:
+        reasonsforinvalid = validateemail(email)
+        for i in reasonsforinvalid:
+            print(i)
         email = input("Please enter your email:\n")
+        attempts -= 1
+    try:
+        newuser = MiniProjectPart1.UserAccount(username, password, email, listofusers)
+    except ValueError:
+        print("The user account could not be created, please try again.")
+        
     
 def validateemail(email):
+    reasons = []
     if re.search(r"$(\w |(-|_|\.)(\w))+@[A-Za-z](\w |-\w)+\.[A-Za-z]{2,}", email) == None:
-        return False
+        reasons.append("This email has an invalid format.")
+        return reasons
     return email
         
 def validateusername(username):
+    reasons = []
     if re.search(r"[\w\.-]{5, }", username) == None:
-        print("The username contains invalid character or is of the incorrect length.")
-        return False
+        if len(username) < 5:
+            reasons.append("The username is too short.")
+        if re.search(r"[^\w\.-]", username):
+            reasons.append("The username contains invalid characters.")
+        return reasons
     return username
 
 def validatepassword(password):
+    reasons = []
     if re.search(r"[\w\.\+-*=#\(\)&!?%]{8, }"):
         print("The password is invalid because it is too short or contains invalid characters.")
-        return False
+        return reasons
     return password
 
     

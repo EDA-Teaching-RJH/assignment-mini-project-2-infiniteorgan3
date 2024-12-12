de02fc09a0802bdc9bec86ae67b51ee1792ca979
Ellie Marsden <ejam2@kent.ac.uk>
2024-12-12 10:14:42 +0000

    aaaaa

--- MiniProjectPart2.py
+++ MiniProjectPart2.py
@@ -103,7 +103,7 @@ def validateemail(email):
         
 def validateusername(username):
     reasons = []
-    if re.search(r"^[-\w\.]{5,}$", username) == None:
+    if re.search(r"^[-\w\.]{5, }$", username) == None:
         if len(username) < 5:
             reasons.append("The username is too short.")
         if re.search(r"(.)*[^\w.-](.)*", username):
@@ -113,10 +113,10 @@ def validateusername(username):
         return username
 
 def validatepassword(password):
-    if re.search(r"[\w.+-*=#()&!?%]{8, }") == None:
+    if re.search(r"[\w.+-*=#()&!?%]{8, }", password) == None:
         if len(password) < 8:
             print("The password is invalid because it is too short.")
-        if re.search(r"(.)*[^\w\.+-*=#()&!?%](.)*"):
+        if re.search(r"(.)*[^\w\.+-*=#()&!?%](.)*", password):
             print("The password is invalid because it contains invalid characters.")
         return None
     else:

b3620427577b2b17ad0c6e0c0e8bd3c46a22416f
Ellie Marsden <ejam2@kent.ac.uk>
2024-12-12 10:50:02 +0000

    aaa

--- MiniProjectPart2.py
+++ MiniProjectPart2.py
@@ -107,7 +107,7 @@ def validateusername(username):
     if re.search(regexpattern, username) == None:
         if len(username) < 5:
             reasons.append("The username is too short.")
-        if re.search(r"(.)*[^\w.-](.)*", username):
+        if re.search(r"(.)*[^\w.](.)*", username):
             reasons.append("The username contains invalid characters.")
         return reasons
     else:
